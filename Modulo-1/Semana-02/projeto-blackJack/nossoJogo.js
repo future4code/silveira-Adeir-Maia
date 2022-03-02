857905bdb471054f105c36ef6dbb4a43a91c4fee
Adeir Maia <adeir.maia@hotmail.com>
2022-03-02 00:28:41 -0300

    função puxar cartas e verificar cartas repetidas

--- Modulo-1/Semana-02/projeto-blackJack/nossoJogo.js
+++ Modulo-1/Semana-02/projeto-blackJack/nossoJogo.js
@@ -1,34 +1,23 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-   
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
-import comprarCarta from "./naoMexer.js";
+import puxar from "./puxar.js";
 
 console.log('Boas vindas ao jogo de Blackjack!')
 
 if (confirm('Vamos jogar?')) {
    console.log('Todos os jogadores compram 2 cartas')
 
-   const maoJogador = [comprarCarta(), comprarCarta()]
-   const maoComputador = [comprarCarta(), comprarCarta()]
+   let maoJogador = [], maoComputador = []
+   maoJogador = [puxar(maoJogador), puxar(maoJogador)]
+   maoComputador = [puxar(maoComputador), puxar(maoComputador)]
 
    const pontosJ = maoJogador.reduce((a, e) => a += e.valor, 0)
    const pontosC = maoComputador.reduce((a, e) => a += e.valor, 0)
    //a === acumulador, e === elemento. Callbeck recebe acumlador com valor 0 e elemento, cada elemento.valor é adicionado ao acumulador que depois é retornado.
 
-   console.log(`Mão do Jogador: ${maoJogador[0].texto} ${maoJogador[1].texto} - ${pontosJ}`)
-   console.log(`Mão do Computador: ${maoComputador[0].texto} ${maoComputador[1].texto} - ${pontosC}`)
+   console.log(`Mão do Jogador: ${maoJogador[0].carta} ${maoJogador[1].carta} - ${pontosJ}`)
+   console.log(`Mão do Computador: ${maoComputador[0].carta} ${maoComputador[1].carta} - ${pontosC}`)
 
    if ((pontosJ > pontosC) && pontosJ < 22) console.log(`Parabens! Você venceu!`)
    else if ((pontosC > pontosJ) && pontosC < 22) console.log(`Que pena! Você perdeu!`)
    else if (pontosJ === pontosC || (pontosJ > 21 && pontosC > 21)) console.log(`Deu Empate!`)
 }
+
