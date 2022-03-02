7c490391ef20bb6313a45dbc15bb3008a6aa2852
Adeir Maia <adeir.maia@hotmail.com>
2022-03-02 00:11:05 -0300

    função  puxar cartas e verificar cartas repetidas

--- Modulo-1/Semana-02/projeto-blackJack/desafio.js
+++ Modulo-1/Semana-02/projeto-blackJack/desafio.js
@@ -1,17 +1,4 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-   
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
-import comprarCarta from "./naoMexer.js";
+import puxar from "./puxar.js";
 
 let jogar = true
 
@@ -24,33 +11,38 @@ Vamos jogar?`)) {
 
       let maoJogador = []
       do {
-         maoJogador = [comprarCarta(), comprarCarta()]
-      } while (maoJogador[0].texto.includes('A') && maoJogador[1].texto.includes('A'))
+         maoJogador = [puxar(maoJogador)], maoJogador.push(puxar(maoJogador))
+      } while (maoJogador[0].carta.includes('A') && maoJogador[1].carta.includes('A'))
       //Enquanto os naipes das duas cartas forem "A" o jogador devolve-as para o baralho e compra 2 novas cartas 
 
+      let cartasEmJogo = [...maoJogador]
       let maoComputador = []
       do {
-         maoComputador = [comprarCarta(), comprarCarta()]
-      } while (maoJogador[0].texto.includes('A') && maoJogador[1].texto.includes('A'))
+         maoComputador = [puxar(cartasEmJogo)], maoComputador.push(puxar(cartasEmJogo.concat(maoComputador)))
+      } while (maoComputador[0].carta.includes('A') && maoComputador[1].carta.includes('A'))
       //Enquanto os naipes das duas cartas forem "A" o computador devolve-as para o baralho e compra 2 novas cartas 
+
+      cartasEmJogo = cartasEmJogo.concat(maoComputador)
 
       const pontosJ = () => maoJogador.reduce((a, e) => a += e.valor, 0)
       const pontosC = () => maoComputador.reduce((a, e) => a += e.valor, 0)
       //a === acumulador, e === elemento. Callbeck recebe acumlador com valor 0 e o elemento, cada elemento.valor é adicionado ao acumulador que depois é retornado.
 
-      const cartasJ = () => maoJogador.map(e => e.texto)
-      const cartasC = () => maoComputador.map(e => e.texto)
+      const cartasJ = () => maoJogador.map(e => e.carta)
+      const cartasC = () => maoComputador.map(e => e.carta)
       // Retorna um array em que cada elemento é o texto de cada carta 
 
 
       while (confirm(`Suas cartas são ${cartasJ()} e o computador mostrou a carta ${cartasC()[0]}
 Quer comprar outra carta?`) && pontosJ() < 21) {
-         maoJogador.push(comprarCarta())
+         maoJogador.push(puxar(maoJogador, cartasEmJogo))
       }
       //Equanto o jogador clicar em ok e sua pontuação for menor do que 21, ele pode comprar cartas
 
+      cartasEmJogo = maoJogador.concat(maoComputador)
+
       while (((pontosC() < pontosJ()) && pontosJ() < 22)) {
-         maoComputador.push(comprarCarta())
+         maoComputador.push(puxar(maoComputador, cartasEmJogo))
       }
       //Equanto a pontuação do computador for menor que a do usuário e a pontuação deste não tiver estourado, o computador pode comprar cartas
 
