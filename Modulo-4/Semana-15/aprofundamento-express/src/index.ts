b736fc0a53c7383961f083b3b6886509a8af3df9
Adeir Maia <adeir.maia@hotmail.com>
2022-06-01 10:24:13 -0300

    mais uns exercícios

--- Modulo-4/Semana-15/aprofundamento-express/src/index.ts
+++ Modulo-4/Semana-15/aprofundamento-express/src/index.ts
@@ -13,6 +13,15 @@ app.get("/ping",(req:Request,res:Response) => res.send("Pong! 🏓"))
 
 app.get("/allTarefas",(req:Request,res:Response) => res.send(tarefas))
 
+app. get("/userTarefa/:id",(req:Request,res:Response)=> {
+    const userId = +req.params.id
+    const selectedUser:Afazeres[] = []
+    const others:Afazeres[] = []
+    tarefas.forEach(tarefa => tarefa.UserId === userId ? selectedUser.push(tarefa): others.push(tarefa))
+    
+    res.status(200).send({todos:{selectedUser,others}})
+})
+
 app.post("/create/tarefa",(req:Request,res:Response) => {
     const tarefasArray = tarefas
     const id = tarefasArray.length + 1 
@@ -24,8 +33,16 @@ app.post("/create/tarefa",(req:Request,res:Response) => {
     res.status(200).send(tarefasArray)
 })
 
-app.put("/change/tarefa",(req:Request,res:Response) => {
+app.put("/change/:id",(req:Request,res:Response) => {
+    const id:number = +req.params.id
+    const tarefasatualizada = tarefas.map(tarefa => tarefa.id === id ? {...tarefa, completed: tarefa.completed ? false : true} : tarefa)
+    res.status(200).send(tarefasatualizada)
+})
 
+app.delete("/delete/:id",(req:Request,res:Response) => {
+    const id:number = +req.params.id
+    const tarefasatualizada = tarefas.filter(tarefa => tarefa.id !== id)
+    res.status(200).send(tarefasatualizada)
 })
 
 app.listen(3005, ()=> console.log('Servidor está rodando na porta http://localhost:3005"'))
