6fc00c3084236b2d8d7ac2ecd5699e5e1a9d5360
Adeir Maia <adeir.maia@hotmail.com>
2022-07-12 16:48:24 -0300

    add atividade

---
+++ Modulo-5/Semana-21/testes-unitarios-mocks/src/index.ts
@@ -0,0 +1,20 @@
+import { Character } from "./types/Types"
+
+export const validateCharacter = (input:Character) => {
+    if(!input.name || !input.life || !input.defense || !input.strength){
+        return false
+    }
+    if(input.life <= 0 || input.strength <= 0 || input.defense <= 0) {
+        return false
+    }
+    return true
+}
+
+export const performAtack = (atacker:Character,defender:Character,validator:(input:Character) => boolean) => {
+    if(!validator(atacker) || ! validateCharacter(defender)) {
+        throw new Error('Atacante ou defensor invalido')
+    }
+    if(atacker.strength > defender.defense) {
+        defender.life -= atacker.strength - defender.defense
+    }
+}
